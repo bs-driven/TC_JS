88202aadd6bcd94e21625ed2b96f66ca3392a368
Bradley <bradsamsess90@icloud.com>
2023-05-04 21:15:34 -0400

    style changes, server.js/package.json/schedule table schema sql start

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE docClient_db;
 USE docClient_db;
 
 CREATE TABLE doctor(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     doc_firstName  VARCHAR(45) NOT NULL,
     doc_lastName VARCHAR(45) NOT NULL,
     doc_email VARCHAR(45) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE doctor(
 );
 
 CREATE TABLE clients (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id DECIMAL(6,4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     client_firstName VARCHAR(35) NOT NULL,
     client_lastName VARCHAR(35) NOT NULL,
     client_phone_number INT NOT NULL,
@@ -23,9 +23,8 @@ CREATE TABLE clients (
 );
 
 CREATE TABLE notes(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    note_date DATE NOT NULL,
-    note_time TIME,
+    note_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    note_date DATETIME,
     note_content TEXT NOT NULL,
     FOREIGN KEY(client_id)
     REFERENCES clients (id)
@@ -39,3 +38,13 @@ CREATE TABLE billing_info(
     FOREIGN KEY(client_id)
     REFERENCES clients (id)
 );
+
+CREATE TABLE scheduled_appointments(
+    appointment_id DECIMAL(7,4),
+    appoimtment_date DATE,
+    appointment_time TIME(2),
+    client_fullName VARCHAR(50)
+    FOREIGN KEY(client_id)
+    REFERENCES clients (id)
+
+)
