469f302e89ee71e1af8147767abb249aff84004e
Bradley <bradsamsess90@icloud.com>
2023-05-17 15:03:20 -0400

    adding the notes and documents to the navbar, schema. added a css starts. no script yet.

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS docClient_db;
 CREATE DATABASE docClient_db;
 USE docClient_db;
 
-CREATE TABLE doctor(
+CREATE TABLE doctor IF NOT EXISTS(
     id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     doc_firstName  VARCHAR(45) NOT NULL,
     doc_lastName VARCHAR(45) NOT NULL,
@@ -10,19 +10,27 @@ CREATE TABLE doctor(
 
 );
 
-CREATE TABLE clients (
+CREATE TABLE clients IF NOT EXISTS(
     id DECIMAL(6,4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     client_firstName VARCHAR(35) NOT NULL,
     client_lastName VARCHAR(35) NOT NULL,
     client_phone_number INT NOT NULL,
     client_email VARCHAR(35) NOT NULL,
+    age_client INT,
+    emergency_contantName VARCHAR(70) NOT NULL,
+    emergency_contactPhone INT,
+    religious_preference VARCHAR(70),
+    pronouns VARCHAR(15),
+    client_sex VARCHAR(7)
     doc_id INT NOT NULL,
     FOREIGN KEY (doc_id)
     REFERENCES doctor(id)
+  -- physical address, emergency contact, age, sex, religous preferences, pronouns
+
 
 );
 
-CREATE TABLE notes(
+CREATE TABLE notes IF NOT EXISTS(
     note_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     note_date DATETIME,
     note_content TEXT NOT NULL,
@@ -30,16 +38,16 @@ CREATE TABLE notes(
     REFERENCES clients (id)
 );
 
-CREATE TABLE billing_info(
-    billing_info_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    billing_address INT NOT NULL,
-    billing_card INT,
+CREATE TABLE billing_info IF NOT EXISTS(
+    billing_info_id INT  AUTO_INCREMENT PRIMARY KEY,
+    billing_address INT ,
+    billing_card_number INT,
     cardholder_name VARCHAR(45),
     FOREIGN KEY(client_id)
     REFERENCES clients (id)
 );
 
-CREATE TABLE scheduled_appointments(
+CREATE TABLE scheduled_appointments IF NOT EXISTS(
     appointment_id DECIMAL(7,4),
     appoimtment_date DATE,
     appointment_time TIME(2),
@@ -47,4 +55,26 @@ CREATE TABLE scheduled_appointments(
     FOREIGN KEY(client_id)
     REFERENCES clients (id)
 
+);
+
+CREATE TABLE documents IF NOT EXISTS(
+    releaseDOC_id INT AUTO_INCREMENT PRIMARY KEY,
+    releaseDOC_form TEXT(6000),
+    
+);
+
+CREATE TABLE personal_message IF NOT EXISTS(
+    personal_message_id INT AUTO_INCREMENT PRIMARY KEY,
+    message_sender VARCHAR(50),
+    message_recipient VARCHAR(50)
+    subject_of_discussion VARCHAR(50),
+    actual_message TEXT(2000),
+);
+
+CREATE TABLE SOAP_subjective_objective_assessment_plan IF NOT EXISTS(
+    S_O_A_P_id FLOAT(15,9) AUTO_INCREMENT PRIMARY KEY,
+    soapNote TEXT(6000),
+    soapDate DATE,
+    doctor_signature_soap VARCHAR(50) NOT NULL,
+    
 )
--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -6,33 +6,34 @@ VALUES
 (02, "Mary", "Grandison", "MMGrandi55@gmail.com"),
 (03, "Marie", "Bradley", "msbradley1941@gmail.com"),
 (04, "Lex", "Luther", "lexl88@gmail.com"),
-(05, "Alivin", "Buzzbee", "ab_jr12@gmail.com");
+(05, "Alivin", "Buzzbee", "ab_jr12@gmail.com"),
+(06, "Imari", "Greene", "marig@gmail.com");
 
 INSERT INTO clients( id, client_firstName, client_lastName,client_phone_number, client_email, doc_id)
 VALUES
-(0001, "Andre", "Miller", 504-784-9865,"millerstunna@gmail.com",04),
-(0002, "Earl", "Spencer", 504-899-7841, "kingspencer@icloud.com", 02),
-(0003, "Luffy", "Monkey", 457-789-5546, "piratecrown@gmail.com", 03),
-(0004, "Jason", "Tatum", 652-456-8790, "tatum0@gmail.com", 01),
-(0005, "Julis", "Erving", 986-546-8491,"drinthehouse@gmail.com",05),
-(0006, "Gon","Freaks", 659-541-2133, "imafinddad@gmail.com",02),
-(0007, "Killua", "Zultic", 859-461-2412, "silentdeath@gmail.com", 04),
-(0008, "Vegeta", "Prince", 614-968-3462, "prideofsayian@gmail.com", 03),
-(0009, "Zoro","Swordsmen", 456-894-9873, "bestalive01@gmail.com", 05),
-(00010, "Franky", "Cyborg" 667-956-8751, "shipwright56@gmail.com", 01);
+(008978.2588, "Andre", "Miller", 504-784-9865,"millerstunna@gmail.com",04),
+(000252.9984, "Earl", "Spencer", 504-899-7841, "kingspencer@icloud.com", 02),
+(000398.2203, "Luffy", "Monkey", 457-789-5546, "piratecrown@gmail.com", 03),
+(000412.5123, "Jason", "Tatum", 652-456-8790, "tatum0@gmail.com", 01),
+(000548.2387, "Julis", "Erving", 986-546-8491,"drinthehouse@gmail.com",05),
+(000645.2588, "Gon","Freaks", 659-541-2133, "imafinddad@gmail.com",02),
+(000787.7877, "Killua", "Zultic", 859-461-2412, "silentdeath@gmail.com", 04),
+(000845.2315, "Vegeta", "Prince", 614-968-3462, "prideofsayian@gmail.com", 03),
+(000987.2103, "Zoro","Swordsmen", 456-894-9873, "bestalive01@gmail.com", 05),
+(000104.2117, "Franky", "Cyborg" 667-956-8751, "shipwright56@gmail.com", 01);
 
-INSERT INTO notes( id, note_date, note_content, client_id)
+INSERT INTO notes( note_id, note_date, note_content, client_id)
 VALUES
-(0000001, 4/26/2023, "Seems to have problems with chamge. Adjustment issues",0001),
-(0000002, 4/10/2023,"Anixety & depression", 0002),
-(0000003, 3/16/2023, "schizophrenic", 003),
-(0000004, 11/29/2022, "adjustment issues", 004),
-(0000005, 12/14/2022, "tendency to be too hard on themself", 005),
-(0000006, 3/15/2023, "witness to his bestfriends death", 006),
-(0000007, 2/21/2023, "calls gunfire at night a regular thing", 007),
-(0000008, 3/10/2023, "argues with parents often", 008),
-(0000009, 3/22/2023, "speaks on killing small animals when younger", 009),
-(00000010, 4/19/2023, "tends to isolate at every oppertunity available", 00010)
-(00000011, 4/29/2022, "deals with the scars of abuse as a youth", 0008)
+(00000001, 4/26/2023, "Seems to have problems with chamge. Adjustment issues",000104.2117),
+(00000002, 4/10/2023,"Anixety & depression", 000987.2103),
+(00000003, 3/16/2023, "schizophrenic", 000845.2315),
+(00000004, 11/29/2022, "adjustment issues", 000787.7877),
+(00000005, 12/14/2022, "tendency to be too hard on themself", 000645.2588),
+(00000006, 3/15/2023, "witness to his bestfriends death", 000548.2387),
+(00000007, 2/21/2023, "calls gunfire at night a regular thing", 000412.5123),
+(00000008, 3/10/2023, "argues with parents often", 000398.2203),
+(00000009, 3/22/2023, "speaks on killing small animals when younger", 000252.9984),
+(000000010, 4/19/2023, "tends to isolate at every oppertunity available", 008978.2588)
+(000000011, 4/29/2022, "deals with the scars of abuse as a youth", 008978.2588)
 
-INSERT INTO billing_info()
+INSERT INTO scheduled_appointments(appointment_id, appoimtment_date,appointment_time, client_id)
